8e0a26d3a6a16d530e8b234486db29f43291aa38
[name] <lihong@uccc.cc>
2017-12-22 14:48:42 +0800

    add test

---
+++ IdeaProjects/test/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static  void  main(String[] args){
+        int a = 4;
+        int b = a+1;
+        double c = b+a;
+        System.out.print(b);
+        System.out.print(a);
+        System.out.print(c);
+    }
+
+}
